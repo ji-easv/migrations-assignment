ffb769ea4c58825b7b8a01a429a4310f574ad641
MatejMa2ur <matejmazur@outlook.com>
2025-02-24 12:33:50 +0100

    Add initial database schema and foreign key constraints
    This commit introduces the initial schema for the database, including tables for Courses, Students, and Enrollments. It also adds foreign key constraints and indexes to the Enrollments table to establish relationships with Courses and Students. These changes ensure data integrity and support cascaded updates/deletions.

---
+++ MigrationsAssignment/Migrations/20250224111005_AddForeignKeys.sql
@@ -0,0 +1,41 @@
+﻿BEGIN TRANSACTION;
+CREATE INDEX "IX_Enrollments_CourseId" ON "Enrollments" ("CourseId");
+
+CREATE INDEX "IX_Enrollments_StudentId" ON "Enrollments" ("StudentId");
+
+CREATE TABLE "ef_temp_Enrollments" (
+    "Id" TEXT NOT NULL CONSTRAINT "PK_Enrollments" PRIMARY KEY,
+    "CourseId" TEXT NOT NULL,
+    "Grade" INTEGER NULL,
+    "StudentId" TEXT NOT NULL,
+    CONSTRAINT "FK_Enrollments_Courses_CourseId" FOREIGN KEY ("CourseId") REFERENCES "Courses" ("Id") ON DELETE CASCADE,
+    CONSTRAINT "FK_Enrollments_Students_StudentId" FOREIGN KEY ("StudentId") REFERENCES "Students" ("Id") ON DELETE CASCADE
+);
+
+INSERT INTO "ef_temp_Enrollments" ("Id", "CourseId", "Grade", "StudentId")
+SELECT "Id", "CourseId", "Grade", "StudentId"
+FROM "Enrollments";
+
+COMMIT;
+
+PRAGMA foreign_keys = 0;
+
+BEGIN TRANSACTION;
+DROP TABLE "Enrollments";
+
+ALTER TABLE "ef_temp_Enrollments" RENAME TO "Enrollments";
+
+COMMIT;
+
+PRAGMA foreign_keys = 1;
+
+BEGIN TRANSACTION;
+CREATE INDEX "IX_Enrollments_CourseId" ON "Enrollments" ("CourseId");
+
+CREATE INDEX "IX_Enrollments_StudentId" ON "Enrollments" ("StudentId");
+
+COMMIT;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250224111005_AddForeignKeys', '9.0.2');
+
