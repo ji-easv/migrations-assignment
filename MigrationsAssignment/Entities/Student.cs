d3b5ad52b3c3471320859b54ad593ec74039dd4a
Madsp15 <madsp@hotmail.dk>
2025-02-24 12:54:35 +0100

    Added Birthday to Student

--- MigrationsAssignment/Entities/Student.cs
+++ MigrationsAssignment/Entities/Student.cs
@@ -6,5 +6,6 @@ public class Student
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public required string Email { get; set; }
+    public DateTimeOffset Birthday { get; set; }
     public DateTimeOffset EnrollmentDate { get; set; }
 }
